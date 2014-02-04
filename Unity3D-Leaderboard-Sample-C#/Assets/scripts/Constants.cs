6359ad4111a942ecb2037ac1a07862d4ffa40a63
Akshay Mishra <akshay.mishra@shephertz.co.in>
2014-02-04 16:46:37 +0530

    Unity Sample.
    Signed-off-by: Akshay Mishra <akshay.mishra@shephertz.co.in>

---
+++ Unity3D-Leaderboard-Sample-C#/Assets/scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Constants
+{
+	public string apiKey  ="d794ed6fd8fa49da69e8cb6f3e19ac4a63a22f92d19f1aa7e658ba1d09b645be";						// API key that you have receieved after the success of app creation from AppHQ
+	public string secretKey ="3421b54ec141f0a7605662577a6aea355ba3b97f4d7143697888fa606f7a852b";					// SECRET key that you have receieved after the success of app creation from AppHQ
+	
+	public string gameName ="Unity3dLeaderboard";						// Name of the game which you can create from AppHQ console by clicking 
+	// Business Service -> Game Service -> Game ->3 Add Game
+}
