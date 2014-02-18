a6e429b8a78db03cc05facc0f8ac6c6931196d71
Akshay Mishra <akshay.mishra@shephertz.co.in>
2014-02-17 18:42:55 -0800

    Sample Updated.
    Signed-off-by: Akshay Mishra <akshay.mishra@shephertz.co.in>

--- Unity3D-Leaderboard-Sample-C#/Assets/scripts/Constants.cs
+++ Unity3D-Leaderboard-Sample-C#/Assets/scripts/Constants.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class Constants
 {
-	public string apiKey  ="d794ed6fd8fa49da69e8cb6f3e19ac4a63a22f92d19f1aa7e658ba1d09b645be";						// API key that you have receieved after the success of app creation from AppHQ
-	public string secretKey ="3421b54ec141f0a7605662577a6aea355ba3b97f4d7143697888fa606f7a852b";					// SECRET key that you have receieved after the success of app creation from AppHQ
+	public string apiKey  ="YOUR_API_KEY";						// API key that you have receieved after the success of app creation from AppHQ
+	public string secretKey ="YOUR_SECRET_KEY";					// SECRET key that you have receieved after the success of app creation from AppHQ
 	
-	public string gameName ="Unity3dLeaderboard";						// Name of the game which you can create from AppHQ console by clicking 
+	public string gameName ="YOUR_GAME_NAME";						// Name of the game which you can create from AppHQ console by clicking 
 	// Business Service -> Game Service -> Game ->3 Add Game
 }
